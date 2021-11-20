f0cae7936de5b4e07d8a442f9137e8fb184f3578
Miyam <miyamizuchisato@gmail.com>
2021-11-20 10:46:12 +0800

    add / as index

--- src/main/java/com/zxc/find/recover/config/InterceptorConfig.java
+++ src/main/java/com/zxc/find/recover/config/InterceptorConfig.java
@@ -14,6 +14,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtInterceptor())
                 .addPathPatterns("/**")
+                .excludePathPatterns("/")
                 .excludePathPatterns("/user/**");
     }
 }
--- src/main/java/com/zxc/find/recover/interceptor/JwtInterceptor.java
+++ src/main/java/com/zxc/find/recover/interceptor/JwtInterceptor.java
@@ -3,7 +3,6 @@ package com.zxc.find.recover.interceptor;
 import com.auth0.jwt.exceptions.AlgorithmMismatchException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zxc.find.recover.utils.JwtUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -21,26 +20,28 @@ public class JwtInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("token");
         Map<String, Object> map = new HashMap<>(12);
-        try {
-            JwtUtils.verity(token);
-            return true;
-        } catch (SignatureVerificationException e) {
-            e.printStackTrace();
-            map.put("msg", "无效签名");
-        } catch (TokenExpiredException e) {
-            e.printStackTrace();
-            map.put("msg", "token过期");
-        } catch (AlgorithmMismatchException e) {
-            e.printStackTrace();
-            map.put("msg", "校验算法不一致");
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("msg", "token无效！");
+        if ("".equals(token)) {
+            try {
+                JwtUtils.verity(token);
+                return true;
+            } catch (SignatureVerificationException e) {
+                e.printStackTrace();
+                map.put("msg", "无效签名");
+            } catch (TokenExpiredException e) {
+                e.printStackTrace();
+                map.put("msg", "token过期");
+            } catch (AlgorithmMismatchException e) {
+                e.printStackTrace();
+                map.put("msg", "校验算法不一致");
+            } catch (Exception e) {
+                e.printStackTrace();
+                map.put("msg", "token无效！");
+            }
+            map.put("state", false);
+            String json = new ObjectMapper().writeValueAsString(map);
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(json);
         }
-        map.put("state", false);
-        String json = new ObjectMapper().writeValueAsString(map);
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(json);
         return false;
     }
 }
